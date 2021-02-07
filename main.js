5b031b43c3c163dcf384e9f744002e49b9fa6cee
Adam Paulukanis <adam.paulukanis@gmail.com>
2021-02-07 23:41:08 +0100

    Try to play sound

--- main.js
+++ main.js
@@ -1,8 +1,4 @@
-document.addEventListener('click', function (event) {
-    console.log(event.target);
-});
-
-function testInputTime() {
+(function(){
     var test = document.createElement('input');
     var fallbackPicker = document.querySelector('span.validity');
 
@@ -20,6 +16,40 @@ function testInputTime() {
         fallbackPicker.style.display = 'block';
         //document.querySelector('div.time input[type="time"]').style.width = "100px";
     }
+})();
+
+// play my sound
+/*
+var playButton = document.querySelector('div.progress a');
+var audio = new Audio('./bell.wav');
+
+playButton.addEventListener("click", handlePlayButton, false);
+
+async function playAudio() {
+    try {
+        await audio.load();
+        await audio.play();
+    } catch(err) {
+        console.error(err);
+    }
 }
 
-testInputTime();
+function handlePlayButton() {
+    return playAudio();
+
+    if (audio.paused) {
+        playAudio();
+    } else {
+        audio.pause();
+    }
+}
+*/
+
+var audioElement = document.createElement('audio');
+audioElement.setAttribute('src', './bell.wav');
+audioElement.addEventListener("load", function(ev) {
+    console.log(ev);
+    console.log('bell.wav loaded!');
+    audioElement.play();
+}, true);
+audioElement.load()
