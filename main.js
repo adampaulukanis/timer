d55b2c7843eb01ddc06b3ffc642a494efcfb652f
Adam Paulukanis <adam.paulukanis@gmail.com>
2021-02-10 20:35:35 +0100

    buttons: second commit

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var test = document.createElement('input');
-var audioElement = document.createElement('audio');
+var audioElement = document.querySelector('audio');
 var playStop = document.querySelector('#play-stop');
 var divTime = document.querySelector('div.time');
 var setTime = document.querySelector('#set-time');
@@ -23,51 +23,13 @@ var progress = document.querySelector('div.progress');
     }
 })();
 
-// play my sound
-/*
-var playButton = document.querySelector('div.progress a');
-var audio = new Audio('./bell.wav');
-
-playButton.addEventListener("click", handlePlayButton, false);
-
-async function playAudio() {
-    try {
-        await audio.load();
-        await audio.play();
-    } catch(err) {
-        console.error(err);
+var startStop = document.querySelector('#start-stop');
+startStop.onclick = function (event) {
+    this.classList.toggle('pause');
+    if(this.classList.contains('pause')){
+        audioElement.play();
+    }else{
+        audioElement.pause();
+        audioElement.load(); // Resets the media to the beginning
     }
 }
-
-function handlePlayButton() {
-    return playAudio();
-
-    if (audio.paused) {
-        playAudio();
-    } else {
-        audio.pause();
-    }
-}
-*/
-
-audioElement.setAttribute('src', './bell.wav');
-audioElement.addEventListener("load", function(ev) {
-    console.log(ev);
-    console.log('bell.wav loaded!');
-    audioElement.play();
-}, true);
-audioElement.load()
-
-playStop.onclick = function(event) {
-    event.preventDefault();
-    setTime.style.display = 'none';
-    spanValidity.style.display = 'none';
-    var counter = document.createElement('div');
-    divTime.appendChild(counter);
-    progress.removeChild(playStop);
-    var pause = document.createElement('i');
-    pause.setAttribute('class', 'fa fa-pause');
-    progress.appendChild(pause);
-    counter.innerText = setTime.value;
-}
-
